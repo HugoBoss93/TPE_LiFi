678cfd9fa4114c032677a2b6994296ec4a4891ee
Hugo <hugo.pm@protonmail.com>
2017-11-11 15:48:12 +0100

    Modification des paramètres de communication

--- src/ParametresCommunication.h
+++ src/ParametresCommunication.h
@@ -8,13 +8,13 @@ struct ParametresCommunication
     bool SYSTEME_BLOCS = true;
 
     /// Longueur d'un bloc, en octets
-    int BLOC_LEN = 4;
+    int BLOC_LEN = 1;
 
     /// Délai entre deux blocs, en millisecondes
-    int BLOC_DELAI = 500;
+    int BLOC_DELAI = 30;
 
     /// Délai entre deux bits d'un même bloc
-    int BIT_DELAI = 50;
+    int BIT_DELAI = 15;
 };
 
 #endif //TPE_LIFI_PARAMETRESCOMMUNICATION_H
